616606fe3ce0cbb14505abcd2b825a2c7fcbda65
worksofeddy <worksofeddy@gmail.com>
2024-08-02 20:14:03 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,4 +9,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start = (page - 1) * page_size
     end = start + page_size
-    return (start, end)#!/usr/bin/python3
+    return (start, end)
